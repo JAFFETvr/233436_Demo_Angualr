55a99527aa185596e011f7eb921c44a77aad26f3
Mayito vicente <cv2143304@gmail.com>
2024-09-10 13:09:32 -0600

    first commit

---
+++ src/app/users/services/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from '../modelo/iuser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  url_base: string = "https://jsonplaceholder.typicode.com/users"
+
+  constructor(private _http: HttpClient) {  }
+
+  
+}
---
+++ src/app/users/users-dashboard/users-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-dashboard',
+  templateUrl: './users-dashboard.component.html',
+  styleUrl: './users-dashboard.component.css'
+})
+export class UsersDashboardComponent {
+  user_list =[]
+
+}
